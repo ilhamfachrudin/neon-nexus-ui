024a6e30b1cc84b91a78976bc24a7181f6c17ace
Xeran <75216452+ilhamfachrudin@users.noreply.github.com>
2026-04-06 00:12:39 +0700

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { NeonButton } from './components/Button/Button';
+export type { NeonButtonProps } from './components/Button/Button';
+
+export { NeonCard } from './components/Card/Card';
+export type { NeonCardProps } from './components/Card/Card';
+
+export { NeonBadge } from './components/Badge/Badge';
+export type { NeonBadgeProps } from './components/Badge/Badge';
+
+export { GlitchText } from './components/GlitchText/GlitchText';
+export type { GlitchTextProps } from './components/GlitchText/GlitchText';
